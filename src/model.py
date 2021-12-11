d79a27643c9109949fec11d109c8e44ba8c59cfa
Max Adamski <maxadamski98@gmail.com>
2021-12-11 15:02:57 +0100

    Example utility functions

--- src/model.py
+++ src/model.py
@@ -1,6 +1,34 @@
 import pandas as pd
 import numpy as np
 
-def global_utility(params, variant):
-    # TODO: model equation
-    return 0
+
+def clip(x, mini, maxi):
+    return max(min(x, maxi), mini)
+
+
+def partial_utilities(params: dict, variant: dict):
+    return dict(
+        dating_apps=params['dating_apps_weight'] * variant['dating_apps_percent']/100,
+        university=params['university_weight'] * clip(params['university_scale'] / variant['university_distance'], 0, 1),
+    )
+
+
+def global_utility(params: dict, variant: dict):
+    U = partial_utilities(params, variant)
+    return np.sum(list(U.values()))
+
+
+if __name__ == '__main__':
+    p1 = dict(
+        dating_apps_weight=0.3,
+        university_weight=0.7,
+        university_scale=1000, # meters
+    )
+    v1 = dict(
+        dating_apps_percent=70,
+        university_distance=500,
+    )
+    print('params =', p1)
+    print('variant =', v1)
+    print('u =', partial_utilities(p1, v1))
+    print('U =', global_utility(p1, v1))
