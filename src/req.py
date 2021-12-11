da5149b9980278f7f2ee114f47478ba2857e841c
Max Adamski <maxadamski98@gmail.com>
2021-12-11 12:09:59 +0100

    Add cache

--- src/req.py
+++ src/req.py
@@ -1,37 +1,62 @@
 import requests
 import json
 import os
+import warnings
+import base64
 
 from urllib.parse import urljoin
+from time import time
 
+warnings.filterwarnings("ignore", "1013: InsecureRequestWarning") 
 
 with open("connection.json", "r") as fp:
-    config = json.loads(fp.read())
+    _config = json.loads(fp.read())
 
-cache_dir = config.get("cache_dir", ".cache")
-if not os.path.exists(cache_dir):
-    os.mkdir(cache_dir)
+_cache_dict = {}
+_cache_dir = _config.get("cache_dir", ".cache")
+if not os.path.exists(_cache_dir):
+    os.mkdir(_cache_dir)
 
+
+def _payload_hash(payload: str) -> str:
+    return str(base64.b64encode(payload.encode("utf-8")), "utf-8")
 
 def _api(endpoint: str, payload: dict, base: str = "https://gateway.oapi.bik.pl/") -> str:
     # if endpoint + payload is cached in a file, read and return it
-    headers = {
-        "BIK-OAPI-Key": config["BIK-OAPI-Key"],
-        "Content-Type": "application/json"
-    }
-    response = requests.request(
-        "POST", 
-        urljoin(base, endpoint), 
-        headers=headers, 
-        data=json.dumps(payload),
-        cert=(config["cert-crt"], config["cert-key"]),
+    payload = json.dumps(payload)
+
+    cache_file = os.path.join(_cache_dir, endpoint + _payload_hash(payload))
+    if cache_file in _cache_dict:
+        # level 1 cache
+        #print('L1 CACHE', cache_file)
+        return _cache_dict[cache_file]
+    elif os.path.exists(cache_file):
+        # level 2 cache
+        #print('L2 CACHE', cache_file)
+        with open(cache_file, "r") as f:
+            data = json.load(f)
+            _cache_dict[cache_file] = data
+            return data
+
+    response = requests.request("POST", urljoin(base, endpoint), 
+        headers={
+            "BIK-OAPI-Key": _config["BIK-OAPI-Key"],
+            "Content-Type": "application/json"
+        },
+        data=payload,
+        cert=(_config["cert-crt"], _config["cert-key"]),
         verify=False
     )
     assert response.status_code == 200, f"API Error ({response.status_code}) ¯\_(ツ)_/¯"
     data = response.json()
-    # TODO(max): cache `data` to some file
+
+    _cache_dict[cache_file] = data
+    os.makedirs(os.path.dirname(cache_file), exist_ok=True)
+    with open(cache_file, "w") as f:
+        f.write(json.dumps(data))
+
     return data
-    
+
 
 def _api4_nearest_poi(address: dict, poi_type: str) -> float:
     payload = {
@@ -58,5 +83,17 @@ if __name__ == '__main__':
         "street": "NOWOGRODZKA",
         "buildingNumber": 17
     }
-    print(post_office(address))
+    t = time()
+    a = post_office(address)
+    print(time() - t, 'seconds')
+
+    t = time()
+    b = post_office(address)
+    print(time() - t, 'seconds')
+
+    t = time()
+    c = post_office(address)
+    print(time() - t, 'seconds')
+
+    assert a == b == c
 
